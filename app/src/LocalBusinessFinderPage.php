261fa72aeb9adefdaa957493413da13baf0f522e
Caroline Lundy <carolinegracelundy@gmail.com>
2026-02-15 16:35:52 -0700

    fixing local business finder page controller returning co instead of nz

--- app/src/LocalBusinessFinderPage.php
+++ app/src/LocalBusinessFinderPage.php
@@ -5,9 +5,7 @@ namespace App;
 use Page;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Control\HTTPResponse;
-use SilverStripe\Control\Director;
-use SilverStripe\Core\Injector\Injector;
-use SilverStripe\Control\HTTPClient;
+use SilverStripe\Core\Environment;
 
 class LocalBusinessFinderPage extends Page
 {
@@ -21,32 +19,30 @@ class LocalBusinessFinderPageController extends \PageController
     public function search(HTTPRequest $request): HTTPResponse
     {
         $query = urlencode($request->getVar('q'));
-        $googleKey = 'GOOGLE_PLACES_API_KEY';
-        $geminiKey = 'GEMINI_API_KEY';
+        $googleKey = Environment::getEnv('GOOGLE_PLACES_API_KEY');
+        $geminiKey = Environment::getEnv('GEMINI_API_KEY');
 
-        $client = Injector::inst()->create(HTTPClient::class);
-
-        $googleUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query={$query}+in+Wellington&key={$googleKey}";
-        $googleResponse = $client->get($googleUrl);
-        $googleData = json_decode($googleResponse->getBody(), true);
+        $googleUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query={$query}+in+Wellington+New+Zealand&key={$googleKey}";
+        $googleResponse = file_get_contents($googleUrl);
+        $googleData = json_decode($googleResponse, true);
         $results = $googleData['results'] ?? [];
-
 
         $names = array_column(array_slice($results, 0, 10), 'name');
         $prompt = "Identify which of these are independent NZ small businesses vs international chains. Return ONLY a JSON object like this: {\"local\": [\"Name1\"], \"chains\": [\"Name2\"]}. List: " . implode(', ', $names);
-
-        $geminiUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" . $geminiKey;
-        $geminiPayload = [
+        $geminiUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" . $geminiKey;
+        $geminiPayload = json_encode([
             "contents" => [["parts" => [["text" => $prompt]]]]
-        ];
-
-        $aiResponse = $client->post($geminiUrl, [
-            'Content-Type' => 'application/json'
-        ], [
-            'body' => json_encode($geminiPayload)
         ]);
 
-        $aiResult = json_decode($aiResponse->getBody(), true);
+        $opts = [
+            'http' => [
+                'method' => 'POST',
+                'header' => "Content-Type: application/json\r\n",
+                'content' => $geminiPayload,
+            ]
+        ];
+        $aiResponse = file_get_contents($geminiUrl, false, stream_context_create($opts));
+        $aiResult = json_decode($aiResponse, true);
 
         $cleanJson = $aiResult['candidates'][0]['content']['parts'][0]['text'] ?? '{}';
         $cleanJson = str_replace(['```json', '```'], '', $cleanJson);
